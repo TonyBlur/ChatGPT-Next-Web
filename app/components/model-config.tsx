c1bc28ff57e9b0db62bdd770b01d719164089649
Liu <zehao100@qq.com>
2023-07-21 21:27:24 +0800

    -Expanded max_token limit from 32k to 100k

--- app/components/model-config.tsx
+++ app/components/model-config.tsx
@@ -76,7 +76,7 @@ export function ModelConfigList(props: {
         <input
           type="number"
           min={100}
-          max={32000}
+          max={100000}
           value={props.modelConfig.max_tokens}
           onChange={(e) =>
             props.updateConfig(
